d5ec11640a5ae6ec14ea7b30a3f0eea46f09223e
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-05-03 12:53:22 +0530

    Create Button.jsx

---
+++ Click Events/Button.jsx
@@ -0,0 +1,16 @@
+function Button(){
+    let count=0;
+    const handleClick = (name) => {
+        if(count < 3){
+            count++;
+            console.log(` ${name} you clicked me ${count} time/s`);
+        }
+        else{
+            console.log(`${name} Stop clicking me`);
+        }
+        
+    };
+   
+    return(<button onClick={() => handleClick("Bro")}> Click me</button>);
+}
+export default Button;
