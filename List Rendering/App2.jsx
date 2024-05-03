9f6c8844b3c88888ef99ae2aeb75c597f30119c5
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-05-03 11:42:03 +0530

    Create App2.jsx

---
+++ List Rendering/App2.jsx
@@ -0,0 +1,21 @@
+import List from './List.jsx'
+function App() {
+
+  const fruits = [{id:1,name:"apple",calories:95},
+                  {id:2,name:"Banana",calories:45},
+                  {id:3,name:"cherry",calories:54},
+                  {id:4,name:"dragonFruit",calories:45}];
+
+  const vegetables= [{id:1,name:"Tomato",calories:34},
+                     {id:2,name:"potato",calories:45},
+                     {id:3,name:"Knolkhol",calories:53},
+                     {id:4,name:"garlic",calories:43}];
+  return (
+    <>
+    <List items={fruits} category="Fruits" />
+    <List items={vegetables} category="Vegetables"/>
+    </>
+    
+  );
+}
+export default App
