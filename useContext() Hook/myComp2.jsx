dc3e17b1e7ed72cfd0dd817062cf96dae87e3cb1
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-05-08 12:17:59 +0530

    Create myComp2.jsx

---
+++ useContext() Hook/myComp2.jsx
@@ -0,0 +1,12 @@
+
+import MyComp3 from "./myComp3.jsx";
+function myComp2(){
+    return(
+        <div className="box">
+            <h1>myComp2</h1>
+            <MyComp3 />
+        </div>
+    );
+
+}
+export default myComp2;
