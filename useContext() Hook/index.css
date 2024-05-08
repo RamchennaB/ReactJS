51b991e075e48ac94fd25d2e7561c4717d68c91d
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-05-08 12:18:42 +0530

    Create index.css

---
+++ useContext() Hook/index.css
@@ -0,0 +1,14 @@
+import React, {useContent, useContext} from 'react';
+import {UserContext} from './myComp1.jsx';
+function myComp3(){
+    const user= useContext(UserContext);
+
+    return(
+        <div className="box">
+            <h1>myComp3</h1>
+            <p>{user}</p>
+        </div>
+    );
+
+}
+export default myComp3;
