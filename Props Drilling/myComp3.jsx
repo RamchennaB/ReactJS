604bb6a4eea630c103c40ae167c67212e322c7a7
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-05-08 12:02:21 +0530

    Create myComp3.jsx

---
+++ Props Drilling/myComp3.jsx
@@ -0,0 +1,10 @@
+function myComp3(props){
+    return(
+        <div className="box">
+            <h1>myComp3</h1>
+            <p>{props.user}</p>
+        </div>
+    );
+
+}
+export default myComp3;
