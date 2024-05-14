26842eb911e3d80cf0f2a06a66c7ea51d0fd283d
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-05-14 13:59:44 +0530

    Create Blog.js

---
+++ React-Router-Dom/Blog.js
@@ -0,0 +1,11 @@
+function Blogs(){
+    return(<>
+        <h1>Blog Articles</h1>
+        <ul className="anchor">
+          <li><a href="https://en.wikipedia.org/wiki/Naturopathy">Naturopathy Overview</a></li><br />
+          <li><a href="http://natureinmind.ie/connecting-nature-practice-wind-watching/">Living with nature</a></li>
+        </ul>
+     </>
+    );
+}
+export default Blogs
