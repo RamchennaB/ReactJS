b93813fa57e5a34f9b2d4c058cb647d023eaa097
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-05-14 18:13:53 +0530

    Update Contact.jsx

--- React-Router-Dom/createBrowserRouter/Contact.jsx
+++ React-Router-Dom/createBrowserRouter/Contact.jsx
@@ -1,6 +1,12 @@
 function Contact(){
-    return(
-        <h1>Contacts</h1>
+    return(<>
+        <h1>Contact Us</h1>
+        <h2>+91 9008450486</h2>
+        <h2>tvg2001@gmail.com</h2>
+       <div className='anchor'>
+        <a href="https://www.google.com/maps/dir/13.0419608,77.5046096/tatva+vaidika+grama/@13.0802908,77.4868671,13z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bae23a622112f51:0x15937b2aef100947!2m2!1d77.5515643!2d13.1239075?entry=ttu">Location</a>
+       </div>
+     </>
     );
 }
 export default Contact;
