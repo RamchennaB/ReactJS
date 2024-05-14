124ccb7b760a4aecdcb113ce00386a7a91518a90
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-05-14 18:17:34 +0530

    Update Home.jsx

--- React-Router-Dom/createBrowserRouter/Home.jsx
+++ React-Router-Dom/createBrowserRouter/Home.jsx
@@ -1,10 +1,12 @@
 import { Outlet, Link } from 'react-router-dom';
+import img1 from '../assets/tatva.jpg'
 function Home(){
     return(
         <>
-         <h1>Tatva Vaidika Grama</h1>
-         <p>Tatva Vaidika Grama is a  community concerned with the ancient way of living life.</p>
-        </>
+        <h1>TATVA VAIDIKA GRAMA</h1>
+        <p>Tatva Vadihika Grama is a community that embraces the principles of naturopathy, an ancient medical approach focusing on natural healing and wellness.</p>
+        <img src={img1} alt="Home" className="image"/>
+       </>
     );
 
 }
