87349e80ec0fcabb3d280754e0367fd704d39368
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-05-14 18:14:31 +0530

    Update Blog.jsx

--- React-Router-Dom/createBrowserRouter/Blog.jsx
+++ React-Router-Dom/createBrowserRouter/Blog.jsx
@@ -1,6 +1,12 @@
 function Blog(){
-    return(
+    return(<>
         <h1>Blog Articles</h1>
+        <ul className="anchor">
+          <li><a href="https://en.wikipedia.org/wiki/Naturopathy">Naturopathy Overview</a></li><br />
+          <li><a href="http://natureinmind.ie/connecting-nature-practice-wind-watching/">Living with nature</a></li>
+        </ul>
+     </>
+        
     );
 }
 export default Blog;
