878d0d346a3e2301aa42f445fed04a0ce48f7128
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-05-14 13:56:08 +0530

    Create Home.js

---
+++ React-Router-Dom/Home.js
@@ -0,0 +1,11 @@
+import './Home.css'
+
+function Home(){
+    return(
+        <>
+         <h1>Tatva Vaidhika Grama</h1>
+         <p>Tatva Vadihika Grama is a community that embraces the principles of naturopathy, an ancient medical approach focusing on natural healing and wellness.</p>
+        </>
+    );
+}
+export default Home;
