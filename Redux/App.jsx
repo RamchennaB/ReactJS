f61368f2c34f9db5d4a009c8ed3c414baa8163d2
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-05-15 13:20:09 +0530

    Create App.jsx

---
+++ Redux/App.jsx
@@ -0,0 +1,21 @@
+import { useSelector, useDispatch} from 'react-redux';
+function App(){
+  const counter =useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () =>{
+    dispatch({type: "INC"});
+  };
+  const decrement = () =>{
+    dispatch({type:"DEC"})
+
+  };
+   return(
+      <div>
+        <h1>Counter App</h1>
+        <h2>{counter}</h2>
+        <button onClick={increment}>increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
+   );
+ }
+export default App
